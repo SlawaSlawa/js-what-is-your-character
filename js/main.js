1dd4b3fa13f3f8bc6bdfef2c5eac415da3735020
Slawa <slawapocht4@yandex.ru>
2021-05-02 20:15:53 +0400

    added listener for items

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 'use strict';
+const answerListItems = document.querySelectorAll('.answer-list__item');
 
+
+
+answerListItems.forEach(item => {
+    item.addEventListener('click', event => {
+        const target = event.target;
+
+        answerListItems.forEach(item => {
+            item.classList.remove('answer-list__item--active');
+        });
+        target.classList.add('answer-list__item--active');
+    });
+});
