d641ae6ae63cf33a4486d31f94cc8a847f7492c8
Slawa <slawapocht4@yandex.ru>
2021-05-03 22:45:05 +0400

    added alphabet-array & keysForCharacterTest-array & handler on button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-
 const content = document.querySelector('.content');
 let counterQuestion = 0;
 let counterAnswers = 0;
 let numberOfTest = 0;
+let scores = 0;
 
 function init() {
     renderCard(dbArray);
@@ -14,26 +14,25 @@ function init() {
 // }
 
 function renderCard(data) {
-	const {id, title, questions, answers} = data[numberOfTest];
+    const { id, title, questions, answers } = data[numberOfTest];
 
-	const answerList = document.createElement('ul');
-	answerList.classList.add('answer-list');
+    const answerList = document.createElement('ul');
+    answerList.classList.add('answer-list');
 
-	answers[counterAnswers].forEach( item => {
-	            			answerList.insertAdjacentHTML('beforeend', `
+    answers[counterAnswers].forEach((item, i) => {
+        answerList.insertAdjacentHTML('beforeend', `
 	            				<li class="answer-list__item">
 				                    <span class="answer-list__marker">
-				                        а)
+				                        ${alphabet[i]}
 				                    </span>
 									${item}            						
-                				</li>`
-                			);
-	            		});
+                				</li>`);
+    });
 
-	const card = document.createElement('div');
-	card.classList.add('question');
+    const card = document.createElement('div');
+    card.classList.add('question');
 
-	card.insertAdjacentHTML('beforeend', `
+    card.insertAdjacentHTML('beforeend', `
 				<span class="question__number">${counterQuestion + 1}.</span> 
                 ${questions[counterQuestion]}`);
 
@@ -41,26 +40,70 @@ function renderCard(data) {
 
     content.insertAdjacentElement('afterbegin', card);
     const answersElement = document.querySelector('.content');
-	answersElement.insertAdjacentElement('beforeend', answerList);
-	answersElement.insertAdjacentHTML('beforeend', `
+    answersElement.insertAdjacentElement('beforeend', answerList);
+    answersElement.insertAdjacentHTML('beforeend', `
 			<button class="btn-submit">
 	            Принять
 	        </button>
 	        <div class="result">
 	        </div>
 		`);
+
+    const btnSubmit = document.querySelector('.btn-submit');
+
+    btnSubmit.addEventListener('click', () => {
+        let choice = answerList.querySelector('.answer-list__item--active');
+        choice = choice.getAttribute('data-target').trim();
+        handlerBtnClick(choice, counterQuestion);
+    });
+
+}
+
+function handlerBtnClick(choice, questionNum) {
+    console.log(choice, questionNum);
+    switch (choice) {
+        case 'a':
+        scores += keysForCharacterTest[0];
+            break;
+        case 'б':
+        scores += keysForCharacterTest[1];
+            break;
+        case 'в':
+        scores += keysForCharacterTest[2];
+            break;
+        case 'г':
+        scores += keysForCharacterTest[3];
+            break;
+        case 'д':
+        scores += keysForCharacterTest[4];
+            break;
+        case 'е':
+        scores += keysForCharacterTest[5];
+            break;
+        case 'ж':
+        scores += keysForCharacterTest[6];
+            break;
+
+    }
+    counterQuestion++;
+    counterAnswers++;
+    init();
 }
 
 function startListener() {
-	const answerListItems = document.querySelectorAll('.answer-list__item');
+    const answerListItems = document.querySelectorAll('.answer-list__item');
     answerListItems.forEach(item => {
         item.addEventListener('click', event => {
             const target = event.target;
 
             answerListItems.forEach(item => {
                 item.classList.remove('answer-list__item--active');
+                item.removeAttribute('data-target');
             });
+            const choice = target.querySelector('.answer-list__marker');
             target.classList.add('answer-list__item--active');
+            target.setAttribute('data-target', choice.textContent);
+            // console.log(target);
         });
     });
 }
